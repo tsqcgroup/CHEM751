25bff8597b2b8ced27130aa9d14b17f71ef17bfc
Tosaporn Sattasathuchana <tsatta@hawaii.edu>
2025-08-29 12:27:35 -1000

    Update subgms_mpi.sh

--- script_submit/subgms_mpi.sh
+++ script_submit/subgms_mpi.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 #SBATCH -N 1
 #SBATCH -t 01:00:00
-#SBATCH --partition=shared,kill-shared,exclusive,kill-exclusive
-#SBATCH --mem=40G
-#SBATCH --ntasks-per-node=24
+#SBATCH --acount=chem751 --partition=chem751
+#SBATCH --mem=4G
+#SBATCH --ntasks-per-node=2
+
+##SBATCH --partition=shared,kill-shared,exclusive,kill-exclusive --mem=40G
 
 file=$1
 echo $file
